d0bd3b0aea39f7e6015bcc319a232e3644553cc6
Chamsy Ismail <ischamsy@gmail.com>
2026-06-08 22:51:57 +0100

    added sound effect on pickups

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI; // REQUIRED: You need this to use 'Image'
 
 public class Item : MonoBehaviour
 {
     public int ID; 
     public string Name;
     public int quantity = 1;
+
+    [Header("Audio Settings")]
+    public AudioClip pickupSound;
+    [Range(0f, 1f)] public float volume = 0.8f;
 
     public virtual void UseItem()
     {
@@ -14,10 +17,26 @@ public class Item : MonoBehaviour
 
     public virtual void PickUp()
     {
-        // She uses Image here, but remember to use SpriteRenderer if this is a ground item!
-        Sprite itemIcon = GetComponent<Image>().sprite; 
+        // 1. Play the sound (This spawns an invisible audio object that survives the Destroy command)
+        if (pickupSound != null && Camera.main != null)
+        {
+            AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position, volume);
+        }
 
-        if (ItemPickupUIController.Instance != null)
+        // 2. Safely get the icon from the SpriteRenderer (since it's on the ground, not UI)
+        Sprite itemIcon = null;
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            itemIcon = sr.sprite;
+        }
+        else
+        {
+            Debug.LogWarning("No SpriteRenderer found on " + Name + "!");
+        }
+
+        // 3. Trigger the UI Popup
+        if (ItemPickupUIController.Instance != null && itemIcon != null)
         {
             ItemPickupUIController.Instance.ShowItemPickup(Name, itemIcon);
         }
